008791ff0896f6e9bc1dcbf0ba3ea2505baa2535
fwdAndroid <fwdkaleem@gmail.com>
2024-07-14 15:50:32 +0500

    Firebase Configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:wholesalegrocery/firebase_options.dart';
 import 'package:wholesalegrocery/screens/splash.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
